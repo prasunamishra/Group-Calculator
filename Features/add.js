3b6cb779cd019b9a945bb6052b893e55508a7a2d
Sagya Ghimire <np03cs4s250084@heraldcollege.edu.np>
2026-03-29 08:38:38 +0545

    addittion feature added

---
+++ Features/add.js
@@ -0,0 +1,5 @@
+function add() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    document.getElementById("result").innerText = "Result: " + (a + b);
+}
